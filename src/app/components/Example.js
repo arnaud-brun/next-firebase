d705a467b3196f22eb1471a3aa58c4615f53800a
Arnaud Brun <arnaud.brun.vidal@gmail.com>
2018-10-16 14:14:27 +0200

    Add redux - working with <yarn serve>

---
+++ src/app/components/Example.js
@@ -0,0 +1,20 @@
+import {connect} from 'react-redux'
+import Clock from './Clock'
+import Counter from './Counter'
+
+function Examples ({ lastUpdate, light }) {
+  console.log('Exemples', lastUpdate, light);
+  return (
+    <div>
+      <Clock lastUpdate={lastUpdate} light={light} />
+      <Counter />
+    </div>
+  )
+}
+
+function mapStateToProps (state) {
+  const { lastUpdate, light } = state
+  return { lastUpdate, light }
+}
+
+export default connect(mapStateToProps)(Examples)
--- src/app/pages/index.js
+++ src/app/pages/index.js
@@ -1,8 +1,47 @@
-import React from "react"
-import App from "../components/App"
+// import React from "react"
+// import App from "../components/App"
+//
+// export default () => (
+//   <App>
+//     <p>Index Page</p>
+//   </App>
+// )
 
-export default () => (
-  <App>
-    <p>Index Page</p>
-  </App>
-)
+import React from 'react'
+import {connect} from 'react-redux'
+import {startClock, serverRenderClock} from '../store'
+
+import App from '../components/App'
+import Examples from '../components/Example'
+
+class Index extends React.Component {
+  static getInitialProps ({ reduxStore, req }) {
+    const isServer = !!req
+    if (!isServer) {
+      reduxStore.dispatch(serverRenderClock(isServer))
+    }
+    return {}
+  }
+
+  componentDidMount () {
+    const {dispatch} = this.props
+    this.timer = startClock(dispatch)
+  }
+
+  componentWillUnmount () {
+    clearInterval(this.timer)
+  }
+
+  render () {
+    return (
+      <App>
+        <>
+          <p>Index Page</p>
+          <Examples />
+        </>
+      </App>
+    )
+  }
+}
+
+export default connect()(Index)
